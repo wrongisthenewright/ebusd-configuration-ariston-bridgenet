551c98a09105bd1c213e407968f59d2f8df44ebf
wrongisthenewright <42111723+wrongisthenewright@users.noreply.github.com>
2023-04-01 18:34:40 +0200

    Added writing rules, docs update

--- docker_run_ebusd.sh
+++ docker_run_ebusd.sh
@@ -1,15 +1,34 @@
-#!/bin/bash
-docker pull john30/ebusd
+#|/bin/bash
 docker stop ebusd
 docker rm ebusd
 
-#For DanMan adapter  YOUR_DEVICE_DETAILS=DEVICE_IP_ADDRESS:3333
-docker run -d --name=ebusd --network YOUR_NETWORK -p 8888:8888 -p 8008:8008 \
--v /YOUR_PATH/Ebusd/configuration:/etc/ebusd \
--v /YOUR_PATH/Ebusd/scripts:/scripts \
--v /YOUR_PATH/Docker/Ebusd/html:/var/ebusd/html \
--v /etc/timezone:/etc/timezone:ro \
--v /etc/localtime:/etc/localtime:ro john30/ebusd \
---mqtthost=YOUR_MQTT_HOST --mqttport=YOUR_MQTT_PORT --mqttuser=YOUR_MQTT_USER --mqttpass=YOUR_MQTT_PASS \
---mqttjson --mqttint=/etc/ebusd/mqtt-hassio.cfg --mqtttopic=ebusd/%circuit/%name--scanconfig=full --enablehex --configpath=/etc/ebusd \
--d YOUR_DEVICE_DETAILS --latency=10000
+docker pull john30/ebusd
+
+#scanconfig enabled
+docker run -d --name=ebusd \
+	--network mynetwork \     #Optional, to allow docker name resolution between containers
+	-p 8888:8888 \
+	-p 8008:8008 \
+	-v /..../Ebusd/configuration:/etc/ebusd \   
+	-v /..../Docker/Ebusd/scripts:/scripts \       #Optional, this allows me to execute script directly inside the container
+	-v /..../Docker/Ebusd/html:/var/ebusd/html \   #unused Ebusd Website feature
+	-v /etc/timezone:/etc/timezone:ro \
+	-v /etc/localtime:/etc/localtime:ro \
+	john30/ebusd:latest \
+	--mqtthost=mosquitto \           #here use the MQTT server IP address, here I can use docker name using the --network docker feature
+	--mqttport=1883 \
+	--mqttuser=USERNAME \
+	--mqttpass=PASSWORD \
+	--mqttjson \
+	--mqttint=/etc/ebusd/mqtt-hassio.cfg \
+	--mqtttopic=ebusd/%circuit/%name \
+	--enablehex \
+	--configpath=/etc/ebusd \
+	-d x.x.x.x:3333 \    #Adapt this to your device IP or serial port
+	--receivetimeout=5000 \
+	--latency=20000 \
+	--pollinterval=30 \
+	--sendretries=10 \
+	--acquireretries=5 \
+	--acquiretimeout=20 \
+	--log=all:error
